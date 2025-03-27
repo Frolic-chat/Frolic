c945ee2f8d6b1ff77fb254dfeccffe6e8a8ffcf1
FireUnderTheMountain <tisabelledefoe@gmail.com>
2025-03-27 14:37:06 -0600

    Icon in collapse button to show when you have messages and notes waiting, for when you have your user list hidden.

--- chat/Sidebar.vue
+++ chat/Sidebar.vue
@@ -2,9 +2,24 @@
     <div class="sidebar-wrapper" :class="{open: expanded}">
         <div :class="'sidebar sidebar-' + (right ? 'right' : 'left')">
             <button @click="expanded = !expanded" class="btn btn-secondary btn-xs expander" :aria-label="label">
-                <span :class="'fa fa-fw fa-rotate-270 ' + icon" v-if="right"></span>
-                <span class="fa" :class="{'fa-chevron-down': !expanded, 'fa-chevron-up': expanded}"></span>
-                <span :class="'fa fa-fw fa-rotate-90 ' + icon" v-if="!right"></span>
+                <span v-if="right" class="fa fa-fw fa-rotate-270" :class="icon"></span>
+                <span
+                    v-if="right" v-show="!expanded"
+                    class="fa fa-rotate-270"
+                    :class="{
+                        'fa-fw fa-mail-bulk':    note &&  msg,
+                        'fa-fw fa-sticky-note': !note &&  msg,
+                        'fa-fw fa-envelope':     note && !msg,
+                    }"
+                ></span>
+                <span
+                    class="fa fa-fw"
+                    :class="{
+                        'fa-chevron-down': !expanded,
+                        'fa-chevron-up':    expanded
+                    }"
+                ></span>
+                <span v-if="!right" class="fa fa-fw fa-rotate-90" :class="icon"></span>
             </button>
             <div class="body">
                 <slot></slot>
@@ -15,8 +30,10 @@
 </template>
 
 <script lang="ts">
-    import {Component, Prop, Watch} from '@f-list/vue-ts';
+    import {Component, Prop, Watch, Hook} from '@f-list/vue-ts';
     import Vue from 'vue';
+    import core from './core';
+    import { EventBus } from './preview/event-bus';
 
     @Component
     export default class Sidebar extends Vue {
@@ -34,5 +51,34 @@
         watchOpen(): void {
             this.expanded = this.open;
         }
+
+        note: boolean = false;
+        msg:  boolean = false;
+
+        updateNoteDisplay(): void {
+            const counts = core.siteSession.interfaces.notes.getCounts();
+
+            this.note = counts.unreadNotes    > 0;
+            this.msg  = counts.unreadMessages > 0;
+        };
+
+        @Hook('mounted')
+        mounted(): void {
+            this.updateNoteDisplay();
+
+            EventBus.$on('note-counts-update', this.updateNoteDisplay);
+        }
     }
 </script>
+
+<style lang="scss">
+    .sidebar {
+        button:focus {
+            box-shadow: none;
+        }
+
+        .fa-mail-bulk, .fa-sticky-note, .fa-envelope {
+            color: var(--warning);
+        }
+    }
+</style>
