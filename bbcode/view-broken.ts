b9b3a3c6bf8d19afe06d8168fb8f3cdd5f4d3740
Fire Under the Mountain <tephradefoe@gmail.com>
2025-06-30 19:45:32 -0600

    Store broken pieces of view.ts

--- bbcode/view-broken.ts
+++ bbcode/view-broken.ts
@@ -1,67 +1,14 @@
-import { h, ref, VNode, PropType, nextTick, onUnmounted, FunctionalComponent, ComponentOptionsWithoutProps } from 'vue';
-import { BBCodeElement } from './core';
-import { BBCodeParser } from './parser';
+import {CreateElement, FunctionalComponentOptions, RenderContext, VNode} from 'vue';
+import {DefaultProps, RecordPropsDefinition} from 'vue/types/options'; //tslint:disable-line:no-submodule-imports
+import {BBCodeElement} from './core';
+import {BBCodeParser} from './parser';
+import { h, ref, PropType, nextTick, onUnmounted, FunctionalComponent, ComponentOptionsWithoutProps } from 'vue';
 
 // BBCodeParser returns type HTMLElement, not a VNode
 
-// export const BBCodeView: FunctionalComponent = (parser: BBCodeParser) => (props: { text?: string, unsafeText: string, afterInsert?: (elm: HTMLElement) => void }) => {
-//     const span = h('span', {}, []);
-//     const element = parser.parseEverything(props.text ?? props.unsafeText);
-//     span.children = [element];
-
-//     span.componentOptions?.hooks?.insert = (node: VNode) => {
-//         const elm = node.elm as HTMLElement;
-//         if (elm) {
-//             elm.appendChild(element as Node);
-//             if (props.afterInsert) props.afterInsert(elm);
-//         }
-//     };
-
-//     span.componentOptions?.hooks?.destroy = (node: VNode) => {
-//         const elm = node.elm as HTMLElement;
-//         const bbCodeElement = elm.firstChild as BBCodeElement;
-//         if (bbCodeElement && bbCodeElement.cleanup) bbCodeElement.cleanup();
-//     };
-
-//     return span;
-// }
-
-export const BBCodeView = (parser: BBCodeParser) => {
-    return {
-        props: {
-            text:        { type: String as PropType<string>, default: '' },
-            unsafeText:  { type: String as PropType<string>, default: '' },
-            afterInsert: { type: Function as PropType<(elm: HTMLElement) => void>, default: (elm: HTMLElement) => {} }
-        },
-        setup(  props: { text?: string,
-                         unsafeText: string,
-                         afterInsert: (elm: HTMLElement) => void },
-                _ctx: any) {
-            const parsedContent = parser.parseEverything(props.text ?? props.unsafeText);
-            const span = h('span', {}, props.text ?? props.unsafeText)
-
-            return () => {
-                const span = h('span', {}, props.text || props.unsafeText);
-                nextTick(() => {
-                    if (span.el) {
-                        span.el.appendChild(parser.parseEverything(props.text || props.unsafeText));
-                        props.afterInsert(span.el);
-                    }
-                });
-                onUnmounted(() => {
-                    const element = span.el?.firstChild as BBCodeElement;
-                    if (element.cleanup) element.cleanup();
-                });
-                return span;
-            };
-        }
-    };
-};
-
-
-export const BBCodeView = (parser: BBCodeParser) => ({
+export const BBCodeView = (parser: BBCodeParser): FunctionalComponentOptions<DefaultProps, RecordPropsDefinition<DefaultProps>> => ({
     functional: true,
-    render(context: RenderContext): VNode {
+    render(createElement: CreateElement, context: RenderContext): VNode {
         /*tslint:disable:no-unsafe-any*///because we're not actually supposed to do any of this
         context.data.hook = {
             insert(node: VNode): void {
@@ -83,40 +30,40 @@ export const BBCodeView = (parser: BBCodeParser) => ({
                 if(element.cleanup !== undefined) element.cleanup();
             }
         };
-        const vnode = h('span', context.data);
+        const vnode = createElement('span', context.data);
         vnode.key = context.props.text;
         return vnode;
         //tslint:enable
     }
 });
 
-import { h, ref, PropType, nextTick, onUnmounted, SetupContext } from 'vue';
-import { BBCodeParser } from './parser';
+// import { h, ref, PropType, nextTick, onUnmounted, SetupContext } from 'vue';
+// import { BBCodeParser } from './parser';
 
-export const BBCodeView = (parser: BBCodeParser) => {
-    return {
-        props: {
-            text: { type: String as PropType<string>, default: '' },
-            unsafeText: { type: String as PropType<string>, default: '' },
-            afterInsert: { type: Function as PropType<(element: HTMLElement) => void>, default: () => {} }
-        },
-        setup(props: {text?: string, unsafeText: string, afterInsert: (element: HTMLElement) => void}, _ctx: SetupContext) {
-            const parsedContent = parser.parseEverything(props.text || props.unsafeText);
-            const spanRef = ref<HTMLElement | null>(null);
+// export const BBCodeView = (parser: BBCodeParser) => {
+//     return {
+//         props: {
+//             text: { type: String as PropType<string>, default: '' },
+//             unsafeText: { type: String as PropType<string>, default: '' },
+//             afterInsert: { type: Function as PropType<(element: HTMLElement) => void>, default: () => {} }
+//         },
+//         setup(props: {text?: string, unsafeText: string, afterInsert: (element: HTMLElement) => void}, _ctx: SetupContext) {
+//             const parsedContent = parser.parseEverything(props.text || props.unsafeText);
+//             const spanRef = ref<HTMLElement | null>(null);
 
-            nextTick(() => {
-                if (spanRef.value) {
-                    spanRef.value.innerHTML = parsedContent.outerHTML;
-                    props.afterInsert(spanRef.value);
-                }
-            });
+//             nextTick(() => {
+//                 if (spanRef.value) {
+//                     spanRef.value.innerHTML = parsedContent.outerHTML;
+//                     props.afterInsert(spanRef.value);
+//                 }
+//             });
 
-            onUnmounted(() => {
-                const element = spanRef.value?.firstChild as HTMLElement & { cleanup?: () => void };
-                if (element.cleanup) element.cleanup();
-            });
+//             onUnmounted(() => {
+//                 const element = spanRef.value?.firstChild as HTMLElement & { cleanup?: () => void };
+//                 if (element.cleanup) element.cleanup();
+//             });
 
-            return () => h('span', { ref: spanRef }, []);
-        }
-    };
-};
+//             return () => h('span', { ref: spanRef }, []);
+//         }
+//     };
+// };
