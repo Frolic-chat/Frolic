a9c11707adec6020a77d027fefeb3c1350ef96b4
Fire Under the Mountain <tephradefoe@gmail.com>
2025-07-04 08:14:46 -0600

    Utility: DeepestKeys and DeepestValues types

--- constants/general.ts
+++ constants/general.ts
@@ -18,3 +18,12 @@ export function IncognitoArgFromBrowserPath(path: string): typeof IncognitoMap[k
     return IncognitoMap[Object.keys(IncognitoMap)
             .find(k => path.includes(k)) as keyof typeof IncognitoMap];
 }
+
+// type DeepestKeys<T> = T extends string ? never : {
+//     [K in keyof T & string]: T[K] extends string ? K : DeepestKeys<T[K]>;
+// }[keyof T & string];
+// type ForbiddenKeys = DeepestKeys<typeof Forbidden>;
+// type DeepestValues<T> = T extends object
+//     ? { [K in keyof T]: DeepestValues<T[K]> }[keyof T]
+//     : T;
+// type ForbiddenVAlues = DeepestValues<typeof Forbidden>;
