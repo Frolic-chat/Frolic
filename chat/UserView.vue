5ade02d56300cb2e3c9565893e3096194cb8c909
Fire Under the Mountain <tephradefoe@gmail.com>
2025-07-28 16:05:16 -0600

    Feat: UserView Overhaul:
    * Convert some updates to computed properties
    
    * Handle show/hide directly in userview and allow `hide` vue prop to specify where UserViews can be hidden
    
    * Tie updates to their data events;
    
    * Allow expensive store loading with appropriate setting (resolves bug)
    
    * Tie return typing to storage to explain relationship in-code

--- chat/UserView.vue
+++ chat/UserView.vue
@@ -1,11 +1,12 @@
 <!-- Linebreaks inside this template will break BBCode views
      v-bind character and channel are used in UserMenu handleEvent when crawling up the DOM -->
-<template><span :class="userClass" v-bind:bbcodeTag.prop="'user'" v-bind:character.prop="character" v-bind:channel.prop="channel" @mouseover.prevent="show()" @mouseenter.prevent="show()" @mouseleave.prevent="dismiss()" @click.middle.prevent.stop="toggleStickyness()" @click.right.passive="dismiss(true)" @click.left.passive="dismiss(true)"><img v-if="!!avatar" :src="avatarUrl" class="user-avatar" /><span v-if="!!statusClass" :class="statusClass"></span><span v-if="!!rankIcon" :class="rankIcon"></span><span v-if="!!smartFilterIcon" :class="smartFilterIcon"></span>{{character.name}}<span v-if="!!matchClass" :class="matchClass">{{getMatchScoreTitle(matchScore)}}</span></span></template>
+<template><span v-if="showing" :class="userClass" v-bind:bbcodeTag.prop="'user'" v-bind:character.prop="character" v-bind:channel.prop="channel" @mouseover.prevent="show()" @mouseenter.prevent="show()" @mouseleave.prevent="dismiss()" @click.middle.prevent.stop="toggleStickyness()" @click.right.passive="dismiss(true)" @click.left.passive="dismiss(true)"><img v-if="avatar" :src="avatarUrl" class="user-avatar" /><span v-if="showStatus" :class="statusClass"></span><span v-if="rankIcon" :class="rankIcon"></span><span v-if="smartFilterIcon" :class="smartFilterIcon"></span>{{ character.name }}<span v-if="match" :class="matchInfo.class">{{ matchInfo.title }}</span></span></template>
 
 <script lang="ts">
 import { Component, Hook, Prop, Watch } from '@f-list/vue-ts';
 import Vue from 'vue';
 import {Channel, Character} from '../fchat';
+import { isImportantToChannel } from './conversations';
 import { Score } from '../learn/matcher';
 import core from './core';
 import { EventBus, CharacterScoreEvent } from './preview/event-bus';
@@ -34,102 +35,21 @@ export function getStatusIcon(status: Character.Status): string {
     }
 }
 
-export interface StatusClasses {
-    rankIcon:         string          | null;
-    smartFilterIcon:  string          | null;
-    statusClass:      string          | null;
-    matchClass:       string          | null;
-    matchScore:       string | number | null;
-    userClass:        string;
-    isBookmark:       boolean;
-}
-
-export function getStatusClasses(character:    Character,
-                                 channel:      Channel | undefined,
-                                 showStatus:   boolean,
-                                 showBookmark: boolean,
-                                 showMatch:    boolean
-                                ): StatusClasses {
-
-    let rankIcon:        StatusClasses['rankIcon']        = null;
-    let statusClass:     StatusClasses['statusClass']     = null;
-    let matchClass:      StatusClasses['matchClass']      = null;
-    let matchScore:      StatusClasses['matchScore']      = null;
-    let smartFilterIcon: StatusClasses['smartFilterIcon'] = null;
-
-    if (character.isChatOp) {
-        rankIcon = 'far fa-gem';
-    }
-    else if (channel) {
-        rankIcon = channel.owner === character.name
-            ? 'fa fa-key'
-            : channel.opList.includes(character.name)
-                ? (channel.id.substring(0, 4) === 'adh-' ? 'fa fa-shield-alt' : 'fa fa-star')
-                : null;
+function getMatchScoreTitle(score: number | string | null): string {
+    switch (score) {
+    case 'unicorn':
+        return 'Unicorn';
+    case Scoring.MATCH:
+        return 'Great';
+    case Scoring.WEAK_MATCH:
+        return 'Good';
+    case Scoring.WEAK_MISMATCH:
+        return 'Maybe';
+    case Scoring.MISMATCH:
+        return 'No';
     }
 
-    if (showStatus || character.status === 'crown')
-        statusClass = `fa-fw ${getStatusIcon(character.status)}`;
-
-    let cache: CharacterCacheRecord | null | undefined = undefined;
-    try {
-        /**
-         * In old code, `core.connection.isOpen` is a proxy for `core.state.settings`,
-         * and it works because you're stuck on a load screen for the duration of both
-         * structures setting up. However, this coupling just tip-toes around the issue.
-         * `.settings` *deliberately* throws, so catching is proper and intended.
-         */
-        if (showMatch && core.state.settings.risingAdScore) {
-            // Isn't this supposed to show some sort of matching?
-            cache = core.cache.profileCache.getSync(character.name);
-        }
-        else if (core.state.settings.risingFilter.showFilterIcon) {
-            cache = core.cache.profileCache.getSync(character.name);
-        }
-    }
-    catch {}
-
-    // undefined == not interested
-    // null == no cache hit
-    if (cache === null && showMatch)
-        void core.cache.addProfile(character.name);
-
-    if (cache && showMatch && core.state.settings.risingAdScore) {
-        if (cache.match.searchScore >= kinkMatchWeights.unicornThreshold && cache.match.matchScore === Scoring.MATCH) {
-            matchClass = 'match-found unicorn';
-            matchScore = 'unicorn';
-        }
-        else {
-            matchClass = `match-found ${Score.getClasses(cache.match.matchScore)}`;
-            matchScore = cache.match.matchScore;
-        }
-    }
-
-    if (cache?.match.isFiltered && core.state.settings.risingFilter.showFilterIcon) {
-        smartFilterIcon = 'user-filter fas fa-filter';
-    }
-
-    const gender = (character.overrides.gender || character.gender)?.toLowerCase() ?? 'none';
-
-    let isBookmark: boolean = false;
-    try {
-        if (showBookmark && core.state.settings.colorBookmarks && (character.isFriend || character.isBookmarked)) {
-            isBookmark = true;
-        }
-    }
-    catch {}
-
-    const userClass = `user-view gender-${gender}${isBookmark ? ' user-bookmark' : ''}`;
-
-    return {
-        rankIcon:    rankIcon    ? `user-rank   ${rankIcon}`    : null,
-        statusClass: statusClass ? `user-status ${statusClass}` : null,
-        matchClass,
-        matchScore,
-        userClass,
-        smartFilterIcon,
-        isBookmark,
-    };
+    return '';
 }
 
 @Component({ components: {} })
@@ -137,52 +57,251 @@ export default class UserView extends Vue {
     @Prop({ required: true })
     readonly character!: Character;
 
+    /**
+     * Display moderator markers, crowns, etc.
+     *
+     * Global operator sigils are always shown (so you know you're chatting with a mod)
+     */
     @Prop()
     readonly channel?: Channel;
 
-    @Prop({ default: true })
+    /**
+     * This is exclusively used by the UserList for channels.
+     */
+    @Prop({ default: false })
+    readonly hide: boolean = false;
+
+    /**
+     * Enable the status icon.
+     */
+    @Prop({ default: false })
     readonly showStatus: boolean = true;
 
+    /**
+     * Color username special for friends/bookmarks
+     */
     @Prop({ default: true })
     readonly bookmark: boolean = true;
 
+    /**
+     * Display match-quality text ("Great", "Good", etc.)
+     */
     @Prop({ default: false })
     readonly match: boolean = false;
 
+    /**
+     * Display user info in the image-preview popup on mouseover.
+     */
     @Prop({ default: true })
     readonly preview: boolean = true;
 
+    /**
+     * Display the user avatar
+     *
+     * Used in private message header and search results
+     */
     @Prop({ default: false })
     readonly avatar: boolean = false;
 
-    userClass:       StatusClasses['userClass']       = '';
-    rankIcon:        StatusClasses['rankIcon']        = null;
-    statusClass:     StatusClasses['statusClass']     = null;
-    matchClass:      StatusClasses['matchClass']      = null;
-    matchScore:      StatusClasses['matchScore']      = null;
-    smartFilterIcon: StatusClasses['smartFilterIcon'] = null;
+    cache?: CharacterCacheRecord | null;
+    scoreWatcher: ((e: CharacterScoreEvent) => Promise<void>) | null = null;
+    settingsWatcher: (() => Promise<void>) | null = null;
 
-    avatarUrl:       string | null                    = null;
+    characterUrl = '';
+    getCharacterUrl(): UserView['characterUrl'] { return `flist-character://${this.character.name}` }
 
-    scoreWatcher: ((event: CharacterScoreEvent) => void) | null = null;
+    avatarUrl = '';
+    getAvatarUrl(): UserView['avatarUrl'] { return characterImage(this.character.name) }
+
+    /** Utility */
+    hiding = false;
+    getHiding(): UserView['hiding'] {
+        try {
+            // Try may be unnecessary because hiding is only enabled from places where you're logged in.
+            // Still: every proxy is a bad proxy.
+            if (this.hide && this.channel && core.state.settings.risingFilter.hideChannelMembers)
+                return true;
+        }
+        catch {}
+
+        return false;
+    }
+
+    matching = false;
+    getMatching(): UserView['matching'] {
+        try {
+            if (this.match && core.state.settings.risingAdScore)
+                return true;
+        }
+        catch {}
+
+        return false;
+    }
+    /** /Utility */
+
+    showing = false;
+    getShowing(): UserView['showing'] {
+        if (this.hiding && this.cache?.match.isFiltered && !isImportantToChannel(this.character, this.channel!))
+            return false;
+        else
+            return true;
+    }
+
+    smartFilterIcon: string | undefined = '';
+    getSmartFilterIcon(): UserView['smartFilterIcon'] {
+        if (core.state.settings.risingFilter.showFilterIcon && this.cache?.match.isFiltered && !(this.character.isFriend || this.character.isBookmarked)) {
+            return 'user-filter fas fa-filter';
+        }
+        else {
+            return;
+        }
+    }
+
+    userClass = '';
+    getUserClass(): UserView['userClass'] {
+        let bookmark: string = '';
+        try {
+            // We may not even need to `try` by checking `isFriend|isBookmarked` first.
+            if (this.bookmark && (this.character.isFriend || this.character.isBookmarked) && core.state.settings.colorBookmarks) {
+                bookmark = 'user-bookmark';
+            }
+        }
+        catch {}
+
+        const gender = ((this.character.overrides.gender || this.character.gender) ?? 'None').toLowerCase();
+
+        return `user-view gender-${gender} ${bookmark}`;
+    }
+
+    // Like the avatarUrl override, these only update when the character data does - so they don't need individual watchers.
+    // @Watch('character.gender')
+    // updateGender(): void         { this.getUserClass() }
+    // @Watch('character.overrides.gender')
+    // updateOverrideGender(): void { this.getUserClass() }
+    @Watch('character.isFriend')
+    updateFriend(): void         { this.getUserClass() }
+    @Watch('character.isBookmarked')
+    updateBookmark(): void       { this.getUserClass() }
+
+    // This one shouldn't be reactive, but how else do we update it? The userlist won't update it.
+    rankIcon: string | undefined = '';
+    getRankIcon(): UserView['rankIcon'] {
+        if (this.character.isChatOp) {
+            return 'user-rank far fa-gem';
+        }
+        else if (this.channel) {
+            if (this.channel.owner === this.character.name) {
+                return 'user-rank fa fa-key';
+            }
+            else if (this.channel.opList.includes(this.character.name)) {
+                if (this.channel.id.substring(0, 4) === 'adh-')
+                    return 'user-rank fa fa-shield-alt';
+                else
+                    return 'user-rank fa fa-star';
+            }
+        }
+    }
+
+    @Watch('character.isChatOp')
+    updateChatOp():        void { this.rankIcon = this.getRankIcon() }
+    @Watch('channel.owner')
+    updateChannelOwner():  void { this.rankIcon = this.getRankIcon() }
+    @Watch('channel.opList')
+    updateChannelOpList(): void { this.rankIcon = this.getRankIcon() }
+
+    get statusClass() {
+        if (this.showStatus || this.character.status === 'crown')
+            return 'user-status fa-fw ' + getStatusIcon(this.character.status);
+        else
+            return;
+    }
+
+    matchInfo: { class?: string, title?: string } = {};
+    getMatchInfo(): UserView['matchInfo'] {
+        if (!this.matching || !this.cache)
+            return {};
+
+        const perfect_match = this.cache.match.searchScore >= kinkMatchWeights.unicornThreshold && this.cache.match.matchScore === Scoring.MATCH;
+
+        return {
+            class: perfect_match
+                ? 'match-found unicorn'
+                : 'match-found ' + Score.getClasses(this.cache.match.matchScore),
+            title: perfect_match
+                ? 'Unicorn'
+                : getMatchScoreTitle(this.cache.match.matchScore),
+        }
+    }
+
+    /**
+     * Update properties reliant on character data/cache
+     */
+    async updateCharacter(): Promise<void> {
+        if (!this.cache && (this.matching || this.hiding || core.state.settings.risingFilter.showFilterIcon)) {
+            if (core.state.settings.expensiveMemberList)
+                this.cache = await core.cache.profileCache.get(this.character.name);
+            else
+                this.cache = core.cache.profileCache.getSync(this.character.name);
+        }
+
+
+        this.characterUrl = this.getCharacterUrl();
+        this.avatarUrl = this.getAvatarUrl();
+
+        this.rankIcon = this.getRankIcon();
+        this.matchInfo = this.getMatchInfo();
+
+        this.showing = this.getShowing();
+    }
+
+    /**
+     * Properties to update when the user settings change
+     */
+    async updateSettings(): Promise<void> {
+        //await Promise.resolve(); // no-op await if necessary
+        this.hiding = this.getHiding();
+        this.matching = this.getMatching();
+        this.userClass = this.getUserClass();
+    }
+
+    /**
+     * Update properties depending both on character cache and settings
+     */
+    updateBoth(): void {
+        //await Promise.resolve(); // no-op await if necessary
+        this.smartFilterIcon = this.getSmartFilterIcon();
+    }
 
     @Hook('mounted')
     onMounted(): void {
-        this.update();
+        // Character-based updates depend on `hiding` and `matching` so settings-based updates have to run first.
+        this.chunkProcessor();
+    }
 
-        if (this.match && !this.matchClass) {
-            if (this.scoreWatcher)
-                EventBus.$off('character-score', this.scoreWatcher);
+    registerEvents(): void {
+        this.settingsWatcher = async (): Promise<void> => {
+            await this.updateSettings();
+            await this.updateBoth();
 
-            this.scoreWatcher = ({ profile }): void => {
-                if (profile.character.name === this.character.name) {
-                    this.update();
+            // Additional hooks?
+        };
 
-                    if (this.scoreWatcher) {
-                        EventBus.$off('character-score', this.scoreWatcher);
+        EventBus.$on('configuration-update', this.settingsWatcher);
 
-                        this.scoreWatcher = null;
-                    }
+        if (this.scoreWatcher)
+            EventBus.$off('character-score', this.scoreWatcher);
+
+        if (!this.cache) {
+            this.scoreWatcher = async ({ profile }): Promise<void> => {
+                if (this.scoreWatcher && profile.character.name === this.character.name) {
+                    await this.updateCharacter();
+                    await this.updateBoth();
+
+                    if (this.hiding && this.cache?.match.isFiltered)
+                        this.$emit('visibility-change');
+
+                    EventBus.$off('character-score', this.scoreWatcher);
+                    this.scoreWatcher = null;
                 }
             };
 
@@ -190,71 +309,59 @@ export default class UserView extends Vue {
         }
     }
 
+    async chunkProcessor(): Promise<void> {
+        setTimeout(async () => {
+            this.updateSettings();
+            setTimeout(async () => {
+                await this.updateCharacter();
+                setTimeout(() => {
+                    this.updateBoth();
+                    setTimeout(() => {
+                        this.registerEvents();
+                    });
+                });
+            });
+        });
+    }
+
+    /**
+     * We rely on the scoreWatcher to pick up the score update from `addProfile()`, since `cache` is null when it fails `getSync()`, so it won't update computed values.
+     */
+    cacheHit(): void {
+        try {
+            if (this.matching || this.hiding || core.state.settings.risingFilter.showFilterIcon) {
+                this.cache = core.cache.profileCache.getSync(this.character.name);
+
+                if (!this.cache)
+                    void core.cache.addProfile(this.character.name);
+            }
+        }
+        catch {}
+    }
+
     @Hook('beforeDestroy')
     onBeforeDestroy(): void {
-        if (this.scoreWatcher)
-            EventBus.$off('character-score', this.scoreWatcher);
+        if (this.scoreWatcher)    EventBus.$off('character-score',      this.scoreWatcher);
+        if (this.settingsWatcher) EventBus.$off('configuration-update', this.settingsWatcher);
 
         this.dismiss();
     }
 
-    @Hook('deactivated')
-    deactivate(): void { this.dismiss() }
-
-    @Hook('beforeUpdate')
-    onBeforeUpdate(): void { this.update() }
-
-    @Watch('character.status')
-    onStatusUpdate(): void { this.update() }
-
-    @Watch('character.overrides.avatarUrl')
-    onAvatarUrlUpdate(): void { this.update() }
-
-    update(): void {
-        const res = getStatusClasses(this.character, this.channel, !!this.showStatus, !!this.bookmark, !!this.match);
-
-        this.rankIcon        = res.rankIcon;
-        this.smartFilterIcon = res.smartFilterIcon;
-        this.statusClass     = res.statusClass;
-        this.matchClass      = res.matchClass;
-        this.matchScore      = res.matchScore;
-        this.userClass       = res.userClass;
-        this.avatarUrl       = this.character.overrides.avatarUrl || characterImage(this.character.name);
-    }
-
-    getMatchScoreTitle(score: number | string | null): string {
-        switch (score) {
-        case 'unicorn':
-            return 'Unicorn';
-        case Scoring.MATCH:
-            return 'Great';
-        case Scoring.WEAK_MATCH:
-            return 'Good';
-        case Scoring.WEAK_MISMATCH:
-            return 'Maybe';
-        case Scoring.MISMATCH:
-            return 'No';
-        }
-
-        return '';
-    }
-
-    getCharacterUrl(): string { return `flist-character://${this.character.name}` }
-
     dismiss(force: boolean = false): void {
         if (this.preview)
-            EventBus.$emit('imagepreview-dismiss', { url: this.getCharacterUrl(), force });
+            EventBus.$emit('imagepreview-dismiss', { url: this.characterUrl, force });
     }
 
     show(): void {
+        this.cacheHit();
 
         if (this.preview)
-            EventBus.$emit('imagepreview-show', { url: this.getCharacterUrl() });
+            EventBus.$emit('imagepreview-show', { url: this.characterUrl });
     }
 
     toggleStickyness(): void {
         if (this.preview)
-            EventBus.$emit('imagepreview-toggle-sticky', { url: this.getCharacterUrl() });
+            EventBus.$emit('imagepreview-toggle-sticky', { url: this.characterUrl });
     }
 }
 </script>
