0aab22dfd86e3fae4e73a6ba4caebd1867987b05
Fire Under the Mountain <tephradefoe@gmail.com>
2025-07-30 11:19:44 -0600

    Feat-ish: Beginning of custom gender

--- chat/common.ts
+++ chat/common.ts
@@ -65,6 +65,7 @@ export class Settings implements ISettings {
     risingShowPortraitNearInput = true;
     risingShowPortraitInMessage = true;
     risingShowHighQualityPortraits = true;
+    customGender = true;
 
     risingFilter = {
         hideAds: false,
