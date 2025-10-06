59e1d9c84ffb6da7a3367b686ecab34b31d976ef
Fire Under the Mountain <tephradefoe@gmail.com>
2025-10-06 17:32:05 -0600

    Volume settings arrangement

--- chat/SettingsView.vue
+++ chat/SettingsView.vue
@@ -238,15 +238,15 @@
                     {{l('rising.preview.showLinkPopup')}}
                 </label>
                 <template v-if="risingLinkPreview">
+                    <template v-if="linkPreviewVolume >= 1">
+                        <br>
+                        <small>{{ l('settings.linkPreviewVolume.warning') }}</small>
+                    </template>
                     <br>
                     <label class="control-label" for="linkPreviewVolume">
                         {{ l('settings.linkPreviewVolume', linkPreviewVolume) }}
                     </label>
                     <input type="range" class="form-control-range" id="linkPreviewVolume" v-model="linkPreviewVolume">
-                    <template v-if="linkPreviewVolume >= 1">
-                        <br>
-                        <small>{{ l('settings.linkPreviewVolume.warning') }}</small>
-                    </template>
                 </template>
             </div>
 
