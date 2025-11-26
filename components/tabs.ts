060e4f4ae2a1349c64dd7336c929e2a5b7ec11be
Fire Under the Mountain <tephradefoe@gmail.com>
2025-11-26 13:29:51 -0700

    Custom styling for tabs.

--- components/tabs.ts
+++ components/tabs.ts
@@ -1,14 +1,28 @@
 import Vue, { CreateElement, VNode } from 'vue';
 
-//tslint:disable-next-line:variable-name
+interface ClassProps {
+    navClasses:  string;
+    listClasses: string;
+    itemClasses: string;
+    linkClasses: string;
+}
+
 const Tabs = Vue.extend({
-    props: [ 'value', 'tabs' ],
+    props: {
+        'value': [ String, Number ],
+        'tabs': { required: false },
+        // Optional bootstrap customization
+        navClasses:  { type: String, default: 'nav-tabs-scroll' }, // div
+        listClasses: { type: String, default: 'nav nav-tabs'    }, // ul
+        itemClasses: { type: String, default: 'nav-item'        }, // li
+        linkClasses: { type: String, default: 'nav-link'        }, // a
+    },
     render( this: Vue & {
                 readonly value?: string,
                 _v?: string,
                 selected?: string,
                 tabs: { readonly [key: string]: string; };
-            },
+            } & ClassProps,
             createElement: CreateElement
         ): VNode {
             let children: { [key: string]: string | VNode | undefined; };
@@ -38,18 +52,18 @@ const Tabs = Vue.extend({
 
             return createElement(
                 'div',
-                { staticClass: 'nav-tabs-scroll' },
+                { staticClass: this.navClasses },
                 [ createElement(
                     'ul',
-                    { staticClass: 'nav nav-tabs' },
+                    { staticClass: this.listClasses },
                     keys.map(key => createElement(
                         'li',
-                        { staticClass: 'nav-item' },
+                        { staticClass: this.itemClasses },
                         [ createElement(
                             'a',
                             {
                                 attrs:       { href: '#' },
-                                staticClass: 'nav-link',
+                                staticClass: this.linkClasses,
                                 class:       { active: this._v === key },
                                 on:          { click: () => this.$emit('input', key) }
                             },
