f8cf4838f03701426d7b847b000c11255a540ad1
Fire Under the Mountain <tephradefoe@gmail.com>
2025-12-13 01:14:37 -0700

    Beginning of in-app log adjustments

--- devtools/log_viewer.vue
+++ devtools/log_viewer.vue
@@ -3,9 +3,9 @@
 <div id="body">
     <div>Levels: {{ levels.join(', ') }}.</div>
     <hr />
-    <div class="row" v-for="level, scope in scopes" :key="scope">
-        <input type="range" v-model="scopes[scope]" :min="0" :max="5" @input="updateLogLevel(scope, level)"  />
-        <div style="flex-grow: 1; margin-left: 13px">{{ levels[level] }}</div>
+    <div class="row" v-for="_, scope in scopes" :key="scope">
+        <input type="checkbox" :value="scopes[scope]" @change="updateLogLevel(scope, $event)"  />
+        <!-- <div style="flex-grow: 1; margin-left: 13px">{{ levels[level] }}</div> -->
         <div style="min-width: 33%">{{ scope }}</div>
     </div>
 </div>
@@ -15,19 +15,40 @@
 import Vue from 'vue';
 import { Component, Hook } from '@f-list/vue-ts';
 
+import core from '../chat/core';
+
 import Logger from 'electron-log/renderer';
 
 @Component
 export default class LogViewer extends Vue {
 levels: string[] = Logger.levels;
-scopes: Record<string, number> = { 'CharacterSearch': 0, 'Chat': 0, 'event-bus': 0, 'chat': 0, 'Index': 0, 'blocker': 0, 'cache-manager': 0, 'matcher': 0, 'UserListSorter': 0, 'WordDefinition': 0, 'note-checker': 0, 'site-session': 0 };
+scopes: Record<string, boolean> = {
+    'CharacterSearch':  false,
+    'Chat':             false,
+    'event-bus':        false,
+    'chat':             false,
+    'Index':            false,
+    'blocker':          false,
+    'cache-manager':    false,
+    'matcher':          false,
+    'UserListSorter':   false,
+    'WordDefinition':   false,
+    'note-checker':     false,
+    'site-session':     false,
+};
 desc: string[] = [];
 logs: { [key: string]: number } = {};
 
 @Hook('created')
-created() {}
+created() {
+    Object.keys(this.scopes).forEach(k => {
+        this.scopes[k] = core.state.generalSettings.argv.includes(`--debug-${k}`);
+    });
+}
 
-updateLogLevel(_scope: string, _level: number) {}
+updateLogLevel(_scope: string, _e: Event) {
+    // const value = e.target.value
+}
 }
 </script>
 
