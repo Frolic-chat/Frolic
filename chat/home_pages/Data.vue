fd76c089276a84f5a7bd85c79dddbc28a8481077
Fire Under the Mountain <tephradefoe@gmail.com>
2025-12-12 18:04:10 -0700

    Scroll to top p2

--- chat/home_pages/Data.vue
+++ chat/home_pages/Data.vue
@@ -182,7 +182,7 @@ export default class Data extends Vue {
             this.loadedLicense.body = await ipcRenderer.invoke('get-text-for-license', f);
             this.loadedLicense.name = f;
 
-            this.$nextTick(() => (this.$refs['licenseView'] as Modal).scrollToTop());
+            (this.$refs['licenseView'] as Modal).scrollToTop();
         }
         catch (e) {
             log.warn('Failed to load license text.', e);
