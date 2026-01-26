ba5f50a6aed1663f84af8421e4596f71309847ba
Fire Under the Mountain <tephradefoe@gmail.com>
2026-01-25 23:28:13 -0700

    Fix import->export scheme broken with upgrade

--- fchat/index.ts
+++ fchat/index.ts
@@ -1,5 +1,7 @@
-export {default as Characters} from './characters';
-export {default as Channels} from './channels';
-export {default as ChatConnection} from './connection';
-export {Connection, Character, Channel, WebSocketConnection} from './interfaces';
-export {decodeHTML} from './common';
+import Characters from './characters';
+import Channels from './channels';
+import ChatConnection from './connection';
+import { Connection, Character, Channel, WebSocketConnection } from './interfaces';
+import { decodeHTML } from './common';
+
+export { Characters, Channels, ChatConnection, Connection, Character, Channel, WebSocketConnection, decodeHTML };
