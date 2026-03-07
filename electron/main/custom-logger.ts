eb60940e5b16efa42b9be34c138869a258a61cdb
Fire Under the Mountain <tephradefoe@gmail.com>
2026-03-06 21:56:50 -0700

    Allow lazy evaluation in logging

--- electron/main/custom-logger.ts
+++ electron/main/custom-logger.ts
@@ -2,6 +2,11 @@
 import Logger from 'electron-log/main';
 import type { LogType } from '../common';
 
+function lazyval(...args: unknown[]): unknown[] {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return args.map(arg => typeof arg === 'function' ? arg() : arg);
+}
+
 /**
  * Ties electron-log to a custom condition. The condition is checked regardless of log level.
  *
@@ -19,13 +24,34 @@ export default function NewLogger(scope: LogType, condition?: (...a: unknown[]) 
 
     const l = Logger.scope(scope);
     return {
-        debug:   (...args: unknown[]) => { if (cond(...args)) l.debug(...args)   },
-        error:   (...args: unknown[]) => { if (cond(...args)) l.error(...args)   },
-        info:    (...args: unknown[]) => { if (cond(...args)) l.info(...args)    },
-        log:     (...args: unknown[]) => { if (cond(...args)) l.log(...args)     },
-        silly:   (...args: unknown[]) => { if (cond(...args)) l.silly(...args)   },
-        verbose: (...args: unknown[]) => { if (cond(...args)) l.verbose(...args) },
-        warn:    (...args: unknown[]) => { if (cond(...args)) l.warn(...args)    },
-        eval:    (...args: unknown[]) => cond(...args),
+        debug:   (...args: unknown[]) => {
+            if (cond(...args))
+                l.debug(  ...lazyval(...args));
+        },
+        error:   (...args: unknown[]) => {
+            if (cond(...args))
+                l.error(  ...lazyval(...args));
+        },
+        info:    (...args: unknown[]) => {
+            if (cond(...args))
+                l.info(   ...lazyval(...args));
+        },
+        log:     (...args: unknown[]) => {
+            if (cond(...args))
+                l.log(    ...lazyval(...args));
+        },
+        silly:   (...args: unknown[]) => {
+            if (cond(...args))
+                l.silly(  ...lazyval(...args));
+        },
+        verbose: (...args: unknown[]) => {
+            if (cond(...args))
+                l.verbose(...lazyval(...args));
+        },
+        warn:    (...args: unknown[]) => {
+            if (cond(...args))
+                l.warn(   ...lazyval(...args));
+        },
+        eval: (...args: unknown[]) => cond(...lazyval(...args)),
     };
 }
--- helpers/log.ts
+++ helpers/log.ts
@@ -4,6 +4,11 @@ import type { LogType } from '../electron/common';
 
 import core from '../chat/core';
 
+function lazyval(...args: unknown[]): unknown[] {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return args.map(arg => typeof arg === 'function' ? arg() : arg);
+}
+
 /**
  * Ties electron-log to a custom condition. The condition is checked regardless of log level.
  *
@@ -22,13 +27,34 @@ export default function NewLogger(scope: LogType, condition?: (...a: unknown[]) 
 
     const l = Logger.scope(scope);
     return {
-        debug:   (...args: unknown[]) => { if (c(...args)) l.debug(...args)   },
-        error:   (...args: unknown[]) => { if (c(...args)) l.error(...args)   },
-        info:    (...args: unknown[]) => { if (c(...args)) l.info(...args)    },
-        log:     (...args: unknown[]) => { if (c(...args)) l.log(...args)     },
-        silly:   (...args: unknown[]) => { if (c(...args)) l.silly(...args)   },
-        verbose: (...args: unknown[]) => { if (c(...args)) l.verbose(...args) },
-        warn:    (...args: unknown[]) => { if (c(...args)) l.warn(...args)    },
-        eval:    (...args: unknown[]) => !!c(...args),
+        debug:   (...args: unknown[]) => {
+            if (c(...args))
+                l.debug(  ...lazyval(...args));
+        },
+        error:   (...args: unknown[]) => {
+            if (c(...args))
+                l.error(  ...lazyval(...args));
+        },
+        info:    (...args: unknown[]) => {
+            if (c(...args))
+                l.info(   ...lazyval(...args));
+        },
+        log:     (...args: unknown[]) => {
+            if (c(...args))
+                l.log(    ...lazyval(...args));
+        },
+        silly:   (...args: unknown[]) => {
+            if (c(...args))
+                l.silly(  ...lazyval(...args));
+        },
+        verbose: (...args: unknown[]) => {
+            if (c(...args))
+                l.verbose(...lazyval(...args));
+        },
+        warn:    (...args: unknown[]) => {
+            if (c(...args))
+                l.warn(   ...lazyval(...args));
+        },
+        eval: (...args: unknown[]) => c(...lazyval(...args)),
     };
 }
