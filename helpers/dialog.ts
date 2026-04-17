1448e94c5b4726821a7758a648ce181e020fbdb8
Fire Under the Mountain <tephradefoe@gmail.com>
2026-04-17 09:52:10 -0600

    Dialog linting/formatting

--- helpers/dialog.ts
+++ helpers/dialog.ts
@@ -2,16 +2,16 @@ import * as remote from '@electron/remote';
 import l from '../chat/localize';
 
 export class Dialog {
-  static confirmDialog(message: string, { yes, no }: { yes?: string, no?: string } = {}): boolean {
-    const result = remote.dialog.showMessageBoxSync({
-      message,
-      title: l('title'),
-      type: 'question',
-      buttons: [yes ?? l('confirmYes'), no ?? l('confirmNo')],
-      defaultId: 1,
-      cancelId: 1
-    });
+    static confirmDialog(message: string, { yes, no }: { yes?: string, no?: string } = {}): boolean {
+        const result = remote.dialog.showMessageBoxSync({
+            message,
+            title:     l('title'),
+            type:      'question',
+            buttons:   [ yes ?? l('confirmYes'), no ?? l('confirmNo') ],
+            defaultId: 1,
+            cancelId:  1,
+        });
 
-    return result === 0;
-  }
+        return result === 0;
+    }
 }
