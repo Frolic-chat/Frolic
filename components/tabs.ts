3d513a5a72b7f597f90faf5e115aa8c751de57ae
Fire Under the Mountain <tephradefoe@gmail.com>
2026-04-21 12:52:20 -0600

    Formatting tabs

--- components/tabs.ts
+++ components/tabs.ts
@@ -8,15 +8,15 @@ interface ClassProps {
 }
 
 interface TooltipData {
-    tooltips?: string[];
+    tooltips?:        string[];
     tooltipPosition?: string;
 }
 
 const Tabs = Vue.extend({
     props: {
-        'value': [ String, Number ],
-        'tabs': { required: false },
-        'tooltips': { type: [ Array ], required: false },
+        'value':           [ String, Number ],
+        'tabs':            { required: false },
+        'tooltips':        { type: [ Array ], required: false },
         'tooltipPosition': { type: String, required: false },
 
         // Optional bootstrap customization
@@ -26,65 +26,65 @@ const Tabs = Vue.extend({
         linkClasses: { type: String, default: 'nav-link'        }, // a
     },
     render( this: Vue & {
-                readonly value?: string,
-                _v?: string,
-                selected?: string,
-                tabs?: { readonly [key: string]: string; };
-            } & ClassProps & TooltipData,
+        readonly value?: string,
+        _v?:             string,
+        selected?:       string,
+        tabs?:           { readonly [key: string]: string; };
+    } & ClassProps & TooltipData,
             createElement: CreateElement
-        ): VNode {
-            let children: { [key: string]: string | VNode | undefined; } = {};
+    ): VNode {
+        let children: { [key: string]: string | VNode | undefined; } = {};
 
-            if (this.$slots['default'] !== undefined) {
-                this.$slots['default'].forEach((child, i) => {
-                    if (child.context !== undefined)
-                        children[child.key !== undefined ? child.key : i] = child;
-                });
-            }
-            else if (this.tabs) {
-                children = this.tabs;
-            }
+        if (this.$slots['default'] !== undefined) {
+            this.$slots['default'].forEach((child, i) => {
+                if (child.context !== undefined)
+                    children[child.key !== undefined ? child.key : i] = child;
+            });
+        }
+        else if (this.tabs) {
+            children = this.tabs;
+        }
 
-            const keys = Object.keys(children);
+        const keys = Object.keys(children);
 
-            if (this._v !== this.value)
-                this.selected = this._v = this.value;
+        if (this._v !== this.value)
+            this.selected = this._v = this.value;
 
-            if (this._v === undefined || children[this._v] === undefined)
-                this.$emit('input', this._v = keys[0]);
+        if (this._v === undefined || children[this._v] === undefined)
+            this.$emit('input', this._v = keys[0]);
 
-            if (this.selected !== this._v && children[this.selected!] !== undefined)
-                this.$emit('input', this._v = this.selected);
+        if (this.selected !== this._v && children[this.selected!] !== undefined)
+            this.$emit('input', this._v = this.selected);
 
-            return createElement(
-                'div',
-                { staticClass: this.navClasses },
-                [ createElement(
-                    'ul',
-                    { staticClass: this.listClasses },
-                    keys.map((key, i) => createElement(
-                        'li',
+        return createElement(
+            'div',
+            { staticClass: this.navClasses },
+            [ createElement(
+                'ul',
+                { staticClass: this.listClasses },
+                keys.map((key, i) => createElement(
+                    'li',
+                    {
+                        staticClass: this.itemClasses,
+                        attrs:       {
+                            'aria-label':       this.tooltips?.[i],
+                            'data-balloon-pos': this.tooltipPosition ?? 'down',
+                        },
+                    },
+                    [ createElement(
+                        'a',
                         {
-                            staticClass: this.itemClasses,
-                            attrs: {
-                                'aria-label': this.tooltips?.[i],
-                                'data-balloon-pos': this.tooltipPosition ?? 'down',
-                            },
-                         },
-                        [ createElement(
-                            'a',
-                            {
-                                attrs:       { href: '#' },
-                                staticClass: this.linkClasses,
-                                class:       { active: this._v === key },
-                                on:          { click: () => this.$emit('input', key) }
-                            },
-                            [ children[key] ]
-                        )]
-                    ))
-                )]
-            );
-    }
+                            attrs:       { href: '#' },
+                            staticClass: this.linkClasses,
+                            class:       { active: this._v === key },
+                            on:          { click: () => this.$emit('input', key) }
+                        },
+                        [ children[key] ]
+                    )]
+                ))
+            )]
+        );
+    },
 });
 
 export default Tabs;
