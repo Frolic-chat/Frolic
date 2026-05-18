aaf1b5fb0574731bbe19df70423832f12706f270
Fire Under the Mountain <tephradefoe@gmail.com>
2026-05-17 20:03:00 -0600

    Allow turning on/off the site message feature from home page settings instead of general settings

--- chat/home_pages/WidgetOptions.vue
+++ chat/home_pages/WidgetOptions.vue
@@ -1,40 +1,41 @@
 <!-- SPDX-License-Identifier: AGPL-3.0-or-later -->
 <template>
-<modal :action="title" ref="dialog" :buttons="false">
-    <div class="d-flex flex-column">
-        <div class="text-center">{{ defaultHome }}</div>
-        <div class="btn-group btn-group-toggle" role="group" aria-label="Default to home toggle">
-            <label class="btn btn-secondary" :class="{ 'active': settings.defaultToHome }">
-                <input type="radio" v-model="settings.defaultToHome" :value="true">
-                {{ home }}
-            </label>
+<modal ref="dialog" :action="title" :buttons="false">
+  <div class="d-flex flex-column">
+    <div class="text-center">{{ defaultHome }}</div>
+    <div class="btn-group btn-group-toggle" role="group" aria-label="Default to home toggle">
+      <label class="btn btn-secondary" :class="{ 'active': settings.defaultToHome }">
+        <input v-model="settings.defaultToHome" type="radio" :value="true">
+        {{ home }}
+      </label>
 
-            <label class="btn btn-secondary" :class="{ 'active': !settings.defaultToHome }">
-                <input type="radio" v-model="settings.defaultToHome" :value="false">
-                {{ console }}
-            </label>
-        </div>
-        <small>{{ homeSwitchTip }}</small>
+      <label class="btn btn-secondary" :class="{ 'active': !settings.defaultToHome }">
+        <input v-model="settings.defaultToHome" type="radio" :value="false">
+        {{ console }}
+      </label>
     </div>
+    <small>{{ homeSwitchTip }}</small>
+  </div>
 
-    <div class="form-group"><hr></div>
+  <div class="form-group"><hr></div>
 
-    <div class="form-group">
-        {{  desc }}
-        <template v-for="(_, k) in widgets">
-            <checkbox :obj="widgets" prefix="widgets" :setting="k"></checkbox>
-        </template>
-    </div>
-
+  <div class="form-group">
+    {{ desc }}
+    <template v-for="(_, k) in availableOptions">
+      <generic-check :key="k" :obj="widgets" prefix="widgets" :setting="k" class="mb-1"></generic-check>
+      <checkbox v-if="k === 'inbox'" :key="`${k}2`" setting="risingShowUnreadOfflineCount" :disabled="!widgets.inbox" class="pl-5"></checkbox>
+    </template>
+  </div>
 </modal>
 </template>
 
 <script lang="ts">
 import { Component } from '@frolic/vue-ts';
 
+import Checkbox from './settings/Checkbox.vue';
 import GenericCheckbox from './settings/GenericCheckbox.vue';
 
-import modal from '../../components/Modal.vue';
+import Modal from '../../components/Modal.vue';
 import CustomDialog from '../../components/custom_dialog';
 
 import core from '../core';
@@ -42,8 +43,9 @@ import l from '../localize';
 
 @Component({
     components: {
-        modal,
-        checkbox: GenericCheckbox,
+        modal:           Modal,
+        checkbox:        Checkbox,
+        'generic-check': GenericCheckbox,
     },
 })
 export default class WidgetOptions extends CustomDialog {
@@ -58,6 +60,20 @@ export default class WidgetOptions extends CustomDialog {
     console     = l('chat.consoleTab');
 
     desc        = l('home.widgetOptions.desc');
+
+    reject: Array<keyof WidgetOptions['widgets']> = [ 'events', 'match', 'suggestions' ];
+
+    get availableOptions() {
+        return (Object.keys(this.widgets) as Array<keyof WidgetOptions['widgets']>).reduce<Partial<WidgetOptions['widgets']>>(
+            (box, widget) => {
+                if (!this.reject.includes(widget))
+                    Object.assign(box, { [widget]: this.widgets[widget] });
+
+                return box;
+            },
+            {}
+        );
+    }
 }
 </script>
 
