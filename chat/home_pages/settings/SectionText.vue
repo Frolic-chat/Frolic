d6bdc646442141b960f70507ef451a74fec55964
Fire Under the Mountain <tephradefoe@gmail.com>
2026-05-17 20:02:11 -0600

    Feat: New section text and section title components for unified style

---
+++ chat/home_pages/settings/SectionText.vue
@@ -0,0 +1,25 @@
+<!-- SPDX-License-Identifier: AGPL-3.0-or-later -->
+<template>
+<div class="form-group form-check settings-search-result-marker">
+  <label v-if="body" class="form-check-label">
+    <slot name="title">{{ body }}</slot>
+  </label>
+  <small v-if="sub" class="help form-text text-muted">
+    <slot name="help">{{ sub }}</slot>
+  </small>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component, Prop } from '@frolic/vue-ts';
+
+@Component({})
+export default class Checkbox extends Vue {
+    @Prop({ default: '' })
+    body!: string;
+
+    @Prop({ default: '' })
+    sub!: string;
+}
+</script>
---
+++ chat/home_pages/settings/SectionTitle.vue
@@ -0,0 +1,32 @@
+<!-- SPDX-License-Identifier: AGPL-3.0-or-later -->
+<template>
+<h5 class="row settings-section-header settings-search-result-marker">
+  <div class="col-auto">{{ title }}</div>
+  <div class="col align-content-center">
+    <div class="hr w-100"></div>
+  </div>
+</h5>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component, Prop } from '@frolic/vue-ts';
+
+@Component({})
+export default class HorizontalRule extends Vue {
+    @Prop({ required: true })
+    title!: string;
+}
+</script>
+
+<style lang="css">
+.settings-section-header:not(:first-child) {
+    margin-top: 1.5rem;
+}
+.settings-section-header .hr {
+    height: 1px;
+    border-top: 1px solid transparent;
+    border-image: linear-gradient(to left, var(--secondary) 90%, transparent 100%);
+    border-image-slice: 1;
+}
+</style>
